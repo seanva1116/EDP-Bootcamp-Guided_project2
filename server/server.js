1ffae48e595eb4c739a11dda55a4ec844cba7f52
Sean Vargas <seanva@me.com>
2024-07-01 16:07:36 -0400

    Add characters, films, and planets path

--- server/server.js
+++ server/server.js
@@ -1,16 +1,54 @@
+import { MongoClient, ObjectId } from 'mongodb';
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const charColl = process.env.CHARACTERs_COLLECTION;
+const filmsColl = process.env.FILMS_COLLECTION;
+const planetsColl = process.env.PLANETS_COLLECTION;
 
 const PORT = 4000;
 const app = express();
 app.use(express.json());
 
-app.get("/api/planets", async (req, res) => {
+app.get("/api/characters", async (req, res) => {
     try {
-        let dummyData = { Name: "test name", data: 42 };
-        res.json(dummyData);
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(charColl);
+        const characters = await collection.find({}).toArray();
+        res.json(characters);
     }
     catch (e) {
-        res.status(500).send(`Error: ${e}`);
+        res.status(500).send(`Error fetching characters: ${e}`);
+    }
+});
+
+app.get("/api/films", async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(filmsColl);
+        const films = await collection.find({}).toArray();
+        res.json(films);
+    }
+    catch (e) {
+        res.status(500).send(`Error fetching films: ${e}`);
+    }
+});
+
+app.get("/api/planets", async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(planetsColl);
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
+    }
+    catch (e) {
+        res.status(500).send(`Error fetching characters: ${e}`);
     }
 });
 
